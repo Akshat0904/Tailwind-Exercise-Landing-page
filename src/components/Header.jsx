056593b88d69dd468f49c8e4cff04ceebe4c1931
Akshat0904 <askansara17@gmail.com>
2024-01-25 15:03:28 +0530

    Header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import LOGO from "/logo.svg";
+import DOT from "/blue-dot.svg";
+
+export default function Header() {
+  return (
+    <header className="">
+      <div className="flex my-29 max-h-16 mx-100 2xl:mx-auto justify-between max-w-1440">
+        <img src={LOGO} alt="Sinar Education Logo" />
+        <nav className="my-5 max-w-584">
+          <ul className="flex font-Poppins text-base font-normal gap-52 text-at-gray-100">
+            <li className="text-at-primary-blue flex flex-col">
+              Home
+              <span className="mx-auto my-2">
+                <img src={DOT} alt="Hover blue dot" />
+              </span>
+            </li>
+            <li>Courses</li>
+            <li>Discover</li>
+            <li>Live lessons</li>
+            <li>My content</li>
+          </ul>
+        </nav>
+        <div className="flex font-Poppins text-base font-normal text-at-primary-blue gap-24">
+          <button>Log in</button>
+          <button className="py-2.5 px-5 border bg-at-white-50 rounded-10 border-at-white-100 shadow-dropShadow  ">
+            Sign up
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        29: "29px",
+        24: "24px",
+        52: "52px",
+        100: "100px",
+        584: "584px",
+        1440: "1440px",
+      },
+      colors: {
+        "at-primary-blue": "#0D154B",
+        "at-gray-100": "#8E8FA5",
+        "at-white-100": "#E6E0EC",
+        "at-white-50": "#FFF",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    boxShadow: {
+      dropShadow:
+        "0px 1px 1px 0px rgba(0, 0, 0, 0.10), 0px 2px 2px 0px rgba(0, 0, 0, 0.09), 0px 5px 3px 0px rgba(0, 0, 0, 0.05), 0px 9px 4px 0px rgba(0, 0, 0, 0.01), 0px 14px 4px 0px rgba(0, 0, 0, 0.00)",
+    },
+    borderRadius: {
+      10: "10px",
+    },
   },
   plugins: [],
 };
