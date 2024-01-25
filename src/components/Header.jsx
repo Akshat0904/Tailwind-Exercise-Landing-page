0003a0bb751a0fcaa557ac2c85e890568e07fd86
Akshat0904 <askansara17@gmail.com>
2024-01-25 15:37:47 +0530

    Header done with responsive ness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import LOGO from "/logo.svg";
 import DOT from "/blue-dot.svg";
+import HAMBURGER from "/hamburger.svg";
 
 export default function Header() {
   return (
-    <header className="">
-      <div className="flex my-29 max-h-16 mx-100 2xl:mx-auto justify-between max-w-1440">
+    <header>
+      <div className="flex my-29 max-h-16 mx-4 md:mx-5 lg:mx-100 2xl:mx-auto justify-between max-w-1440">
         <img src={LOGO} alt="Sinar Education Logo" />
-        <nav className="my-5 max-w-584">
-          <ul className="flex font-Poppins text-base font-normal gap-52 text-at-gray-100">
+        <nav className="my-5 max-w-584 hidden md:block">
+          <ul className="flex font-Poppins text-base font-normal gap-4 lg:gap-29 xl:gap-52 text-at-gray-100">
             <li className="text-at-primary-blue flex flex-col">
               Home
               <span className="mx-auto my-2">
@@ -20,11 +21,14 @@ export default function Header() {
             <li>My content</li>
           </ul>
         </nav>
-        <div className="flex font-Poppins text-base my-2.5 font-normal text-at-primary-blue gap-24">
+        <div className="md:flex font-Poppins text-base my-2.5 font-normal text-at-primary-blue gap-2 lg:gap-24 hidden">
           <button>Log in</button>
           <button className="py-2.5 px-5 border bg-at-white-50 rounded-10 border-at-white-100 shadow-dropShadow  ">
             Sign up
           </button>
+        </div>
+        <div className="md:hidden my-auto">
+          <img src={HAMBURGER} alt="Hamburger icon" />
         </div>
       </div>
     </header>
