994b1fe70fbb5128b05af441557755d72b52d2b2
Akshat0904 <askansara17@gmail.com>
2024-01-29 16:57:58 +0530

    First section is created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,36 @@ import LOGO from "/logo.svg";
 import DOT from "/blue-dot.svg";
 import HAMBURGER from "/hamburger.svg";
 
-export default function Header() {
+const Header = () => {
   return (
-    <header>
-      <div className="flex my-29 max-h-16 mx-4 md:mx-5 lg:mx-100 2xl:mx-auto justify-between max-w-1440">
+    <header className="max-h-123 py-29 border-b border-at-primary-blue ">
+      <div className="flex max-h-16 mx-4 md:mx-5 lg:mx-100 2xl:mx-auto justify-between max-w-1440">
         <img src={LOGO} alt="Sinar Education Logo" />
         <nav className="my-5 max-w-584 hidden md:block">
           <ul className="flex font-Poppins text-base font-normal gap-4 lg:gap-29 xl:gap-52 text-at-gray-100">
             <li className="text-at-primary-blue flex flex-col">
-              Home
-              <span className="mx-auto my-2">
-                <img src={DOT} alt="Hover blue dot" />
+              <span className="mx-auto">
+                <a href="#">Home</a>
+                <img src={DOT} className="mx-auto my-2" alt="Hover blue dot" />
               </span>
             </li>
-            <li>Courses</li>
-            <li>Discover</li>
-            <li>Live lessons</li>
-            <li>My content</li>
+            <li>
+              <a href="#">Courses</a>
+            </li>
+            <li>
+              <a href="#">Discover</a>
+            </li>
+            <li>
+              <a href="#">Live lessons</a>
+            </li>
+            <li>
+              <a href="#">My content</a>
+            </li>
           </ul>
         </nav>
         <div className="md:flex font-Poppins text-base my-2.5 font-normal text-at-primary-blue gap-2 lg:gap-24 hidden">
           <button>Log in</button>
-          <button className="py-2.5 px-5 border bg-at-white-50 rounded-10 border-at-white-100 shadow-dropShadow  ">
+          <button className="py-2.5 px-5 border rounded-lg bg-at-white-50  border-at-white-100 shadow-dropShadow  ">
             Sign up
           </button>
         </div>
@@ -33,4 +41,6 @@ export default function Header() {
       </div>
     </header>
   );
-}
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,53 @@ export default {
   theme: {
     extend: {
       spacing: {
+        17: "17px",
+        19: "19px",
         29: "29px",
         24: "24px",
+        44: "44px",
         52: "52px",
+        60: "60px",
+        69: "69px",
+        77: "77px",
         100: "100px",
+        123: "123px",
+        158: "158px",
+        187: "187px",
+        195: "195px",
+        207: "207px",
+        284: "284px",
+        310: "310px",
+        351: "351px",
+        356: "356px",
+        552: "552px",
         584: "584px",
+        749: "749px",
+        607: "607px",
         1440: "1440px",
       },
       colors: {
         "at-primary-blue": "#0D154B",
+        "at-primary-white": "#F7F8FD",
         "at-gray-100": "#8E8FA5",
+        "at-gray-200": "#383A4D",
+        "at-gray-500": "#252739",
         "at-white-100": "#E6E0EC",
         "at-white-50": "#FFF",
+        "at-blue-100": "#56C6E7",
+        "at-purple-100": "#8A6CF9",
+      },
+      blur: {
+        "5xl": "200px",
+      },
+      backgroundImage: {
+        "h1-gradient":
+          "linear-gradient(90deg, #6F48FC 21.63%, #D07EF6 60.55%, #56C6E7 79.11%)",
       },
     },
+    // fontSize: {
+    //   57: "57px",
+    // },
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
     },
@@ -26,7 +59,21 @@ export default {
         "0px 1px 1px 0px rgba(0, 0, 0, 0.10), 0px 2px 2px 0px rgba(0, 0, 0, 0.09), 0px 5px 3px 0px rgba(0, 0, 0, 0.05), 0px 9px 4px 0px rgba(0, 0, 0, 0.01), 0px 14px 4px 0px rgba(0, 0, 0, 0.00)",
     },
     borderRadius: {
-      10: "10px",
+      lg: "10px",
+      12: "12px",
+      99: "99px",
+      310: "310px",
+      552: "552px",
+    },
+    // borderWidth: {
+    //   10: "10px",
+    // },
+    letterSpacing: {
+      small: "-0.25px",
+    },
+    lineHeight: {
+      32: "32px",
+      64: "64px",
     },
   },
   plugins: [],
